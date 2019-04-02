28905ed57214125a910c935a02d28e5c77104a4f
heavyjg <heavyjg@gmail.com>
2019-04-02 07:41:21 -0300

    inserindo v-flex

--- helmet-app/src/components/CadastroColaboradores.vue
+++ helmet-app/src/components/CadastroColaboradores.vue
@@ -1,14 +1,20 @@
 <template>
 <v-form v-model="valid" ref="form" lazy-validation>
-  <h2>Cadastro de colaboradores</h2>
-  <v-text-field label="Nome do colaborador" v-model="nome" :rules="nameRules" required></v-text-field>
-  <v-text-field label="Cargo do colaborador" v-model="cargo" :rules="cargoRules" required></v-text-field>
-  <v-text-field label="CPF do colaborador" v-model="cpf" :mask="cpfMask" :rules="cpfRules" required></v-text-field>
-  <v-text-field name="input-7-1" label="Função do colaborador" v-model="description" multi-line></v-text-field>
-  <v-btn @click="submit" color="info">
-    Cadastrar
-  </v-btn>
-  <v-btn @click="clear">Limpar</v-btn>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs8 offset-xs2>
+        <h2>Cadastro de colaboradores</h2>
+        <v-text-field label="Nome do colaborador" v-model="nome" :rules="nameRules" required></v-text-field>
+        <v-text-field label="Cargo do colaborador" v-model="cargo" :rules="cargoRules" required></v-text-field>
+        <v-text-field label="CPF do colaborador" v-model="cpf" :mask="cpfMask" :rules="cpfRules" required></v-text-field>
+        <v-text-field name="input-7-1" label="Função do colaborador" v-model="description" multi-line></v-text-field>
+        <v-btn @click="submit" color="info">
+          Cadastrar
+        </v-btn>
+        <v-btn @click="clear">Limpar</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </v-form>
 </template>
 <script>
